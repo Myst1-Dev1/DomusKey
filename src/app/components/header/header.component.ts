9af8d69157cd1740e13e360a7bf4b71a7a3a9ccb
João Victor <joao.victor@internit.net.br>
2025-04-23 22:21:23 -0300

    adjusts

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [FontAwesomeModule],
   standalone:true,
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  faBars = faBars;
 
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
--- src/app/components/home/faq/faq.component.ts
+++ src/app/components/home/faq/faq.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import { NgClass, NgFor, NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-faq',
-  imports: [FontAwesomeModule],
+  imports: [NgClass, NgFor, NgStyle, FontAwesomeModule],
   templateUrl: './faq.component.html',
   styleUrl: './faq.component.scss'
 })
 export class FaqComponent {
   faChevronDown = faChevronDown;
+
+  questions = [
+    { title: 'Como inicio o processo de compra de um imóvel com a DomusKey', text: 'Lorem Ipsum...' },
+    { title: 'Que tipos de propriedade a DomusKey oferece?', text: 'Lorem Ipsum...' },
+    { title: 'A DomusKey pode ajudar com o financiamento imobiliário ou hipotecas?', text: 'Lorem Ipsum...' }
+  ];  
+
+  activeIndex: number | null = null;
+
+  toggleAccordion(index: number): void {
+    this.activeIndex = this.activeIndex === index ? null : index;
+  }
 }
