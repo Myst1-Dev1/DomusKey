7645d490fcdee0602504a2f10477d68f26abc6ad
João Victor <joao.victor@internit.net.br>
2025-04-24 16:20:58 -0300

    swiper has added and video logic to play after view in windows

--- src/app/components/home/immobiles/immobiles.component.ts
+++ src/app/components/home/immobiles/immobiles.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import {
@@ -13,14 +13,61 @@ import {
 @Component({
   selector: 'app-immobiles',
   imports: [NgOptimizedImage, FontAwesomeModule],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './immobiles.component.html',
   styleUrl: './immobiles.component.scss'
 })
-export class ImmobilesComponent {
+export class ImmobilesComponent implements AfterViewInit {
+  @ViewChild('swiperRef', { static: true }) swiperRef!: ElementRef;
+  @ViewChild('prevEl', { static: true }) prevEl!: ElementRef;
+  @ViewChild('nextEl', { static: true }) nextEl!: ElementRef;
+
   faMapMarker = faMapMarkerAlt;
   faBed = faBed;
   faBath = faBath;
   faVectorSquare = faVectorSquare;
   faArrowLeft = faArrowLeft;
   faArrowRight = faArrowRight;
+
+  immobiles = [
+    {
+      img: '/images/property.webp',
+      title:'Tasman Aris Estate',
+      location: 'São Gonçalo - RJ',
+      bedNumbers:'4',
+      bathNumbers: '3',
+      propertySize: '410'
+    },
+    {
+      img: '/images/property.webp',
+      title:'Tasman Aris Estate',
+      location: 'São Gonçalo - RJ',
+      bedNumbers:'4',
+      bathNumbers: '3',
+      propertySize: '410'
+    },
+    {
+      img: '/images/property.webp',
+      title:'Tasman Aris Estate',
+      location: 'São Gonçalo - RJ',
+      bedNumbers:'4',
+      bathNumbers: '3',
+      propertySize: '410'
+    },
+    {
+      img: '/images/property.webp',
+      title:'Tasman Aris Estate',
+      location: 'São Gonçalo - RJ',
+      bedNumbers:'4',
+      bathNumbers: '3',
+      propertySize: '410'
+    },
+  ]
+
+  ngAfterViewInit(): void {
+    const swiperEl = this.swiperRef.nativeElement;
+
+    swiperEl.setAttribute('navigation-prev-el', `.${this.prevEl.nativeElement.classList[1]}`);
+    swiperEl.setAttribute('navigation-next-el', `.${this.nextEl.nativeElement.classList[1]}`);
+  }
 }
--- src/app/components/home/video/video.component.ts
+++ src/app/components/home/video/video.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, PLATFORM_ID, ElementRef, AfterViewInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-video',
-  imports: [],
+  standalone: true,
   templateUrl: './video.component.html',
   styleUrl: './video.component.scss'
 })
-export class VideoComponent {
+export class VideoComponent implements AfterViewInit {
+  @ViewChild('videoRef', { static: true }) videoRef!: ElementRef<HTMLVideoElement>;
 
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngAfterViewInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      const video = this.videoRef.nativeElement;
+
+      const observer = new IntersectionObserver(
+        ([entry]) => {
+          if (entry.isIntersecting) {
+            video.play().catch(err => console.warn('Play bloqueado:', err));
+          }
+        },
+        { threshold: 0.5 }
+      );
+
+      observer.observe(video);
+    }
+  }
 }
